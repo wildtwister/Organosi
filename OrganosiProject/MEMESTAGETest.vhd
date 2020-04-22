5fd6262c7ea49755a23fd0480bf2fd0f36955e76
dlytis <64093568+dlytis@users.noreply.github.com>
2020-04-22 21:39:34 +0300

    Add files via upload

---
+++ OrganosiProject/MEMESTAGETest.vhd
@@ -0,0 +1,111 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   20:40:11 04/22/2020
+-- Design Name:   
+-- Module Name:   C:/Users/dlytis/Desktop/organosi1o/organosi1o/MEMESTAGETest.vhd
+-- Project Name:  organosi1o
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: MEMSTAGE
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY MEMESTAGETest IS
+END MEMESTAGETest;
+ 
+ARCHITECTURE behavior OF MEMESTAGETest IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT MEMSTAGE
+    PORT(
+         ByteOp : IN  std_logic;
+         Mem_WrEn : IN  std_logic;
+         ALU_MEM_Addr : IN  std_logic_vector(31 downto 0);
+         MEM_DataIn : IN  std_logic_vector(31 downto 0);
+         MEM_DataOut : OUT  std_logic_vector(31 downto 0);
+         MM_Addr : OUT  std_logic_vector(10 downto 0);
+         MM_WrEn : OUT  std_logic;
+         MM_WrData : OUT  std_logic_vector(31 downto 0);
+         MM_RdData : IN  std_logic_vector(31 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal ByteOp : std_logic := '0';
+   signal Mem_WrEn : std_logic := '0';
+   signal ALU_MEM_Addr : std_logic_vector(31 downto 0) := (others => '0');
+   signal MEM_DataIn : std_logic_vector(31 downto 0) := (others => '0');
+   signal MM_RdData : std_logic_vector(31 downto 0) := (others => '0');
+
+ 	--Outputs
+   signal MEM_DataOut : std_logic_vector(31 downto 0);
+   signal MM_Addr : std_logic_vector(10 downto 0);
+   signal MM_WrEn : std_logic;
+   signal MM_WrData : std_logic_vector(31 downto 0);
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+    -- Clock period definitions
+   constant CLK_period : time := 1000 ns;
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: MEMSTAGE PORT MAP (
+          ByteOp => ByteOp,
+          Mem_WrEn => Mem_WrEn,
+          ALU_MEM_Addr => ALU_MEM_Addr,
+          MEM_DataIn => MEM_DataIn,
+          MEM_DataOut => MEM_DataOut,
+          MM_Addr => MM_Addr,
+          MM_WrEn => MM_WrEn,
+          MM_WrData => MM_WrData,
+          MM_RdData => MM_RdData
+        );
+
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 500ns    
+		ByteOp <= '0';
+		Mem_WrEn <= '0';
+		ALU_MEM_Addr<="00000000000000000000000000000000";
+		MEM_DataIn<="01100000110010001010111110011000";
+		wait for 500 ns;
+		ByteOp <= '0';
+		Mem_WrEn <= '1';
+		ALU_MEM_Addr<="00000000000000000000000000000000";
+		MEM_DataIn<="01100000110010001010111110011000";
+		wait for 500 ns;
+		ByteOp <= '0';
+		Mem_WrEn <= '0';
+		ALU_MEM_Addr<= "00000000000000000000000000000000";
+
+				
+		wait;
+		end process;
+
+	END;
