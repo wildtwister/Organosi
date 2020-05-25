766a610abcd2b6e4b85b73e55ca9f866954c089b
dlytis <64093568+dlytis@users.noreply.github.com>
2020-05-25 14:51:07 +0300

    Add files via upload

---
+++ OrganosiProject/Top_MC_test.vhd
@@ -0,0 +1,91 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   14:45:29 05/25/2020
+-- Design Name:   
+-- Module Name:   E:/Users/dlytis/Documents/organosi2/organosi2/Top_MC_test.vhd
+-- Project Name:  organosi2
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: Top_MC
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY Top_MC_test IS
+END Top_MC_test;
+ 
+ARCHITECTURE behavior OF Top_MC_test IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT Top_MC
+    PORT(
+         RST : IN  std_logic;
+         CLK : IN  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal RST : std_logic := '0';
+   signal CLK : std_logic := '0';
+
+   -- Clock period definitions
+   constant CLK_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: Top_MC PORT MAP (
+          RST => RST,
+          CLK => CLK
+        );
+
+   -- Clock process definitions
+   CLK_process :process
+   begin
+		CLK <= '0';
+		wait for CLK_period/2;
+		CLK <= '1';
+		wait for CLK_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+ 		RST<='1';
+      wait for 10 ns;	
+		RST<='0';
+		wait for 10 ns;	
+
+      wait for CLK_period*10;
+
+      -- insert stimulus here 
+
+      wait;
+   end process;
+
+END;
